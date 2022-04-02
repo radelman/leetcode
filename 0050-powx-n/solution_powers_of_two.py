a1cfd29da420e829e2e697a5bd9a69b1573c045b
Ross Adelman <radelman@gmail.com>
2022-04-02 13:24:38 -0400

    Adding powers of two solution for problem 50.

---
+++ 0050-powx-n/solution_powers_of_two.py
@@ -0,0 +1,46 @@
+class Solution:
+	def myPow(self, x: float, n: int) -> float:
+		try:
+			abs_x = abs(x)
+			abs_n = abs(n)
+			y = 1.0
+			while abs_n > 0:
+				if abs_n & 1 == 1:
+					y = abs_x * y
+				abs_x = abs_x * abs_x
+				abs_n = abs_n >> 1
+				
+			if x < 0.0:
+				if n % 2 == 1:
+					y = -y
+					
+			if n < 0:
+				y = 1.0 / y
+				
+		except:
+			y = 0.0
+			
+		return y
+	
+def main() -> None:
+	test_cases = [
+	(2.0, 10),
+	(-2.0, 11),
+	(2.0, -12),
+	(2.0, 0),
+	(0.0, 10)
+	]
+	
+	solution = Solution()
+	
+	for inputs in test_cases:
+		x, n = inputs
+		
+		reference = pow(x, n)
+		test = solution.myPow(x, n)
+		
+		print(str(test) + ' =? ' + str(reference))
+		
+if __name__ == '__main__':
+	main()
+	
