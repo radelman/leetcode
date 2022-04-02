ddb6e838b7e2f62b52290df7f9a320571e72024a
Ross Adelman <radelman@gmail.com>
2022-04-02 14:51:10 -0400

    Adding and solving problem 53.

---
+++ 0053-maximum-subarray/solution.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+	def maxSubArray(self, nums: List[int]) -> int:
+		n_nums = len(nums)
+		
+		max_overall = nums[0]
+		max_ending_here = nums[0]
+		
+		for i in range(1, n_nums):
+			max_ending_here = max(max_ending_here + nums[i], nums[i])
+			max_overall = max(max_overall, max_ending_here)
+			
+		return max_overall
+	
+def main() -> None:
+	test_cases = [
+	[-2,1,-3,4,-1,2,1,-5,4],
+	[1],
+	[5,4,-1,7,8]
+	]
+	
+	solution = Solution();
+	
+	for inputs in test_cases:
+		nums = inputs
+		
+		test = solution.maxSubArray(nums)
+		
+		print(test)
+		
+if __name__ == '__main__':
+	main()
+	
